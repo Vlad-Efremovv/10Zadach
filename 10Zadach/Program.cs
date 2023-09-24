6112544fdb7aa70ce10a2d04b0897d1ee9a0e339
Владислав Ефремов <vladee.735@mail.ru>
2023-09-24 07:45:18 +0300

    24.09 7;45 ДО 6 КЛАССА

--- 10Zadach/Program.cs
+++ 10Zadach/Program.cs
@@ -39,6 +39,26 @@ namespace _10Zadach
                     Personal_computer personal_computer = new Personal_computer();
                     personal_computer.Info();
                     break;
+                case 4:
+                    Laptop laptop = new Laptop();
+                    laptop.Info();
+                    break;
+                case 5:
+                    Phone phone = new Phone();
+                    phone.Info();
+                    break;
+                case 6:
+                    Rectangle rectangle = new Rectangle();
+                    rectangle.ru[0] = 1;
+                    rectangle.ru[1] = 4;
+
+                    rectangle.ld[0] = 6;
+                    rectangle.ld[1] = -2;
+
+                    rectangle.GetUnknown();
+                    break;
+                case 7:
+                    break;
 
                 default:
                     Console.WriteLine("Ошибка выбора задания!");
@@ -96,6 +116,58 @@ namespace _10Zadach
                 "Обьем жесткого диска - " + volumeHDD + " гигабайт");
         }
 
+    }
+
+    class Laptop
+    {
+        string model = "HUAWEI MateBook D 15";
+        float clockRate = (float)4.3;
+        int volumeRAM = 16;
+        int volumeHDD = 1000;
+        float mass = (float)1.88;
+
+        public void Info()
+        {
+            Console.WriteLine("Ноутбук имеет следующие характеристики:" + "\n" +
+                "Модель ноутбука - " + model + "\n" +
+                "Чатота процессора - " + clockRate + " Ггц" + '\n' +
+                "Обьем оперативной памяти - " + volumeRAM + " гигабайт" + '\n' +
+                "Обьем жесткого диска - " + volumeHDD + " гигабайт \n" +
+                "Масса - " + mass + " кг");
+        }
+    }
+
+    class Phone
+    {
+        string model = "HUAWEI NOVA 5T";
+        float clockRate = (float)2.3;
+        int volumeRAM = 6;
+        int volumeHDD = 128;
+        float mass = (float)300;
+
+        public void Info()
+        {
+            Console.WriteLine("Телефон имеет следующие характеристики:" + "\n" +
+                "Модель ноутбука - " + model + "\n" +
+                "Чатота процессора - " + clockRate + " Ггц" + '\n' +
+                "Обьем оперативной памяти - " + volumeRAM + " гигабайт" + '\n' +
+                "Обьем жесткого диска - " + volumeHDD + " гигабайт \n" +
+                "Масса - " + mass + " грамм");
+        }
+    }
+
+    class Rectangle
+    {
+        public int[] ru = new int[2]; // правый левый
+        public int[] ld = new int[2]; // левый нижний 
+
+        int[] rd = new int[2]; // правый левый
+        int[] lu = new int[2]; // левый нижний 
+
+        public void GetUnknown()
+        {
+            Console.WriteLine(ru[1]);
+        }
     }
 
 }
